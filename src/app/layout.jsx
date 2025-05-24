8957e9bf3e05e85f36ddba6854b762e27681ce26
Tilde Twisttmann <tihe001@stud.kea.dk>
2025-05-24 22:57:48 +0200

    font forsøg

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,28 @@
-import styles from "./layout.module.css"
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
-import "../styles/globals.css"
+import styles from "./layout.module.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "../styles/globals.css";
+
+// Importer fonte via next/font/google
+import { Poppins } from 'next/font/google';
+import { Lexend_Exa } from 'next/font/google';
+
+// Konfigurer fonte og tilføj CSS-variabler
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: [
+    '100', '200', '300', '400', '500', '600', '700', '800', '900',
+  ],
+  variable: '--font-poppins',
+  display: 'swap',
+});
+
+const lexendExa = Lexend_Exa({
+  subsets: ['latin'],
+  weight: ['500'],
+  variable: '--font-lexend-exa',
+  display: 'swap',
+});
 
 export const metadata = {
   title: "DJ Kristian Bak",
@@ -10,12 +31,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="da">
-    <body className={styles.container}>
-      <Navbar />
-      <main>{children}</main>
-      <Footer />
-    </body>
-  </html>
+    <html lang="da" className={`${poppins.variable} ${lexendExa.variable}`}>
+      <body className={styles.container}>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
   );
 }
